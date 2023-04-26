7d6a66fb9af7d9a1fa2082c7c24f5b5b1265bead
hakan <hakancandar@proton.me>
2023-04-26 14:20:39 +0100

    add-element basic functionality

---
+++ src/lib/Element.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let data;
+</script>
+
+{#if data.type === "text"}
+  <textarea class="textarea textarea-bordered w-full" placeholder="text"></textarea>
+{:else if data.type === "image"}
+  <input type="text" placeholder="image address" class="input input-bordered w-full" />
+{:else if data.type === "kvpair"}
+  <div class="flex w-full">
+    <input type="text" placeholder="key" class="grid flex-grow input input-bordered w-full" />
+    <div class="divider divider-horizontal"></div>
+    <input type="text" placeholder="value" class="grid flex-grow input input-bordered w-full" />
+  </div>
+{:else}
+  <div>unknown</div>
+{/if}
+
+
+<style>
+  .textarea {
+    font-size: 1rem;
+  }
+</style>
--- src/pages/Create.svelte
+++ src/pages/Create.svelte
@@ -2,29 +2,55 @@
   import { tick } from "svelte";
   import EditIcon from "../icons/EditIcon.svelte";
   import PlusIcon from "../icons/PlusIcon.svelte";
-    import TextIcon from "../icons/TextIcon.svelte";
-    import ImageIcon from "../icons/ImageIcon.svelte";
-    import HeadingIcon from "../icons/HeadingIcon.svelte";
+  import TextIcon from "../icons/TextIcon.svelte";
+  import ImageIcon from "../icons/ImageIcon.svelte";
+  import HeadingIcon from "../icons/HeadingIcon.svelte";
+  import { element } from "svelte/internal";
+  import Element from "../lib/Element.svelte";
+
+  let elements = [];
+  let counter = 0;
 
   let pageName = "new qrpage";
   let tempName = pageName;
-  let checkboxInput;
+  let nameToggle;
   let nameInput;
   function updateName() {
     pageName = tempName.substring(0, 40);
   }
 
+  let elementToggle;
+  function addElement(type) {
+    counter++;
+    elementToggle.checked = false;
+    elements = elements.concat([
+      {
+        id: counter,
+        value: "",
+        type
+      }
+    ]);
+  }
 
+  let addText = () => addElement("text");
+  let addImage = () => addElement("image");
+  let addKVPair = () => addElement("kvpair");
 </script>
 
 <div class="px-5 mt-16">
-  <div class="paper mx-auto max-w-[650px] text-primary p-4 text-center">
-    <label for="edit-toggle-create" class="normal-case btn btn-ghost mx-auto text-2xl font-bold justify-center items-center inline-flex flex-row space-x-2">
+  <div class="paper mx-auto max-w-[650px] p-4 text-center text-stone-500">
+    <label for="edit-toggle-create" class="normal-case text-primary btn btn-ghost mx-auto text-2xl font-bold justify-center items-center inline-flex flex-row space-x-2">
       <div class="mb-[3px]">{pageName}</div>
       <EditIcon class="w-6 h-6" />
     </label>
+    {#each elements as element (element.id)}
+      <div class="divider my-1"/>
+      <div class="px-2">
+        <Element data={element} />
+      </div>
+    {/each}
     <div class="divider my-1"/>
-    <label for="element-toggle-create" class="normal-case btn btn-ghost w-full flex flex-row space-x-3 items-center justify-start text-stone-500 px-2">
+    <label for="element-toggle-create" class="normal-case btn btn-ghost w-full flex flex-row space-x-3 items-center justify-start px-2">
       <PlusIcon class="w-6 h-6" />
       <div>add element</div>
     </label>
@@ -40,11 +66,11 @@
 
 <!-- BEGIN MONSTROSITY -->
 <!-- SET NAME DIALOG -->
-<input type="checkbox" id="edit-toggle-create" class="modal-toggle" bind:this={checkboxInput} on:change={ e => e.target.checked && setTimeout(() => nameInput.focus(), 100) } />
+<input type="checkbox" id="edit-toggle-create" class="modal-toggle" bind:this={nameToggle} on:change={ e => e.target.checked && setTimeout(() => nameInput.focus(), 100) } />
 <label for="edit-toggle-create" class="modal cursor-pointer">
   <label class="modal-box relative" for="">
     <h3 class="text-lg font-bold">qrpage name</h3>
-    <input type="text" placeholder="Type here" class="input input-bordered w-full block my-4" bind:value={tempName} bind:this={nameInput} on:keypress={(e) => {if (e.key === "Enter") {updateName();  checkboxInput.checked = false}}} />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full block my-4" bind:value={tempName} bind:this={nameInput} on:keypress={(e) => {if (e.key === "Enter") {updateName();  nameToggle.checked = false}}} />
     <div class="modal-action">
       <button on:click={updateName} >
         <label for="edit-toggle-create" class="btn btn-primary">update</label>
@@ -54,31 +80,37 @@
 </label>
 
 <!-- ADD ELEMENT DIALOG -->
-<input type="checkbox" id="element-toggle-create" class="modal-toggle" />
+<input bind:this={elementToggle} type="checkbox" id="element-toggle-create" class="modal-toggle" />
 <label for="element-toggle-create" class="modal cursor-pointer">
   <label class="modal-box relative md:max-w-[45rem]" for="">
     <h3 class="text-lg font-bold">add element</h3>
     <div class="flex flex-col w-full md:flex-row mt-4">
-      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center">
-        <div class="text-center">
-          <HeadingIcon class="w-12 h-12 mx-auto"/>
-          <div>heading</div>
-        </div>
-      </button>
-      <div class="divider md:divider-horizontal" />
-      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center">
+      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center" on:click={addText}>
         <div>
           <TextIcon class="w-12 h-12 mx-auto"/>
           <div>text</div>
         </div>
-      </button> 
+      </button>
       <div class="divider md:divider-horizontal" />
-      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center">
+      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center" on:click={addImage}>
         <div>
           <ImageIcon class="w-12 h-12 mx-auto"/>
           <div>image</div>
         </div>
       </button> 
+      <div class="divider md:divider-horizontal" />
+      <button class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center text-center" on:click={addKVPair}>
+        <div class="text-center">
+          <HeadingIcon class="w-12 h-12 mx-auto"/>
+          <div>key value pair</div>
+        </div>
+      </button>
     </div>
   </label>
 </label>
+
+<style>
+  button.flex-grow {
+    flex-basis: 100%;
+  }
+</style>
