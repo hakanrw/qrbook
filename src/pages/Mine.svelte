b49d5086672f02ab61960e71141e55ecfa9952c1
hakan <hakancandar@proton.me>
2023-06-18 17:20:09 +0100

    add print to my posts page

--- src/pages/Mine.svelte
+++ src/pages/Mine.svelte
@@ -4,7 +4,8 @@
   import { auth, firestore, user } from "../lib/firebase";
   import { Link } from "svelte-navigator";
   import Qr from "../lib/QR.svelte";
-    import TrashIcon from "../icons/TrashIcon.svelte";
+  import TrashIcon from "../icons/TrashIcon.svelte";
+    import PrintIcon from "../icons/PrintIcon.svelte";
 
   let pages = [];
  
@@ -28,12 +29,22 @@
       <Link to={"/" + page.id} class="flex-1">
         <div class="paper  p-4 text-stone-500">
           <div class="flex items-center">
+            <div class="only-on-print h-[100px] w-[100px]"></div>
             <div class="flex-1 text-center text-primary text-xl font-bold">{page.title}</div>
             <Qr address={window.location.hostname + "/" + page.id} height="100" width="100"/>
           </div>
         </div>
       </Link>
-      <button><TrashIcon class="w-8 h-8"/></button>
+      <button class="hidden-print"><TrashIcon class="w-8 h-8"/></button>
     </div>
+    <div class="text-center my-4 only-on-print">{window.location.hostname + "/" + page.id}</div>
   {/each}
 </div>
+
+  
+<button on:click={() => window.print()} class="paper mt-16 w-[200px] mx-auto hidden-print block">
+  <div class="btn btn-ghost flex flex-row space-x-3 items-center justify-center px-2 font-bold hidden-print">
+    <PrintIcon class="w-6 h-6"/>
+    <div>print page</div>
+  </div>
+</button>
